b10019017bbf4f3c6338e7e95131c02bdc6481ca
SHiNKi <shinki.5ktri@gmail.com>
2024-05-12 21:57:45 +0900

    テストコード追加

---
+++ DiscordRx.UnitTest/AppSettingsFixture.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordRx.UnitTest
+{
+    public class AppSettingsFixture : IDisposable
+    {
+        public IConfigurationRoot Configuration { get; private set; }
+        public AppSettingsFixture()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                ;
+            
+            Configuration = builder.Build();
+        }
+
+        public void Dispose()
+        {
+               
+        }
+    }
+}
