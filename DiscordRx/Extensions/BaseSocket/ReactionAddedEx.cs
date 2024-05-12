b7c9ff33db168ced50ea7025ee01f604d8022172
SHiNKi <shinki.5ktri@gmail.com>
2024-05-12 17:54:13 +0900

    ReactionAdded

---
+++ DiscordRx/Extensions/BaseSocket/ReactionAddedEx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using DiscordRx.Arguments;
+
+namespace DiscordRx.Extensions.BaseSocket
+{
+    /// <summary>
+    /// <seealso cref="BaseSocketClient.ReactionAdded"/> 拡張メソッド定義
+    /// </summary>
+    public static class ReactionAddedEx
+    {
+        private static IObservable<ReactionAddedArgs> NotifyReactionAdded(this BaseSocketClient client,
+            Func<Action<ReactionAddedArgs>, Func<Cacheable<IUserMessage, ulong>, Cacheable<IMessageChannel, ulong>, SocketReaction, Task>> conversion)
+        {
+            return Observable.FromEvent(conversion,
+                h => client.ReactionAdded += h,
+                h => client.ReactionAdded -= h
+                );
+        }
+
+        /// <summary>
+        /// メッセージにリアクションが追加されると通知されます。
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
+        public static IObservable<ReactionAddedArgs> NotifyReactionAdded(this BaseSocketClient client)
+        {
+            return client.NotifyReactionAdded(h => (message, channel, reaction) =>
+            {
+                h(new ReactionAddedArgs()
+                {
+                    Message = message,
+                    Channel = channel,
+                    Reaction = reaction
+                });
+                return Task.CompletedTask;
+            });
+        }
+    }
+}
