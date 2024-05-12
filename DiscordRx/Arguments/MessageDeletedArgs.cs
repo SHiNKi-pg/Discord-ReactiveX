4f8d6219e75539984e627d6a220eeb213b5b6e66
SHiNKi <shinki.5ktri@gmail.com>
2024-05-12 23:15:11 +0900

    MessageDeleted

---
+++ DiscordRx/Arguments/MessageDeletedArgs.cs
@@ -0,0 +1,26 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordRx.Arguments
+{
+    /// <summary>
+    /// <seealso cref="BaseSocketClient.MessageDeleted"/> イベント引数用クラス
+    /// </summary>
+    public class MessageDeletedArgs
+    {
+        /// <summary>
+        /// 削除されたメッセージ
+        /// </summary>
+        public required Cacheable<IMessage, ulong> Message { get; init; }
+
+        /// <summary>
+        /// 削除されたメッセージのチャンネル
+        /// </summary>
+        public required Cacheable<IMessageChannel, ulong> Channel { get; init; }
+    }
+}
---
+++ DiscordRx/Extensions/BaseSocket/MessageDeleted.cs
@@ -0,0 +1,46 @@
+﻿using Discord;
+using Discord.WebSocket;
+using DiscordRx.Arguments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordRx.Extensions.BaseSocket
+{
+    /// <summary>
+    /// <seealso cref="BaseSocketClient.MessageDeleted"/> 拡張メソッド
+    /// </summary>
+    public static class MessageDeleted
+    {
+        private static IObservable<MessageDeletedArgs> NotifyMessageDeleted(this BaseSocketClient socketClient,
+            Func<Action<MessageDeletedArgs>, Func<Cacheable<IMessage, ulong>, Cacheable<IMessageChannel, ulong>, Task>> conversion)
+        {
+            return Observable.FromEvent(conversion,
+                h => socketClient.MessageDeleted += h,
+                h => socketClient.MessageDeleted -= h);
+        }
+
+        /// <summary>
+        /// メッセージが削除されると通知されます。
+        /// </summary>
+        /// <param name="socketClient"></param>
+        /// <returns></returns>
+        public static IObservable<MessageDeletedArgs> NotifyMessageDeleted(this BaseSocketClient socketClient)
+        {
+            return socketClient.NotifyMessageDeleted(h => async (message, channel) =>
+            {
+                var messageTask = message.GetOrDownloadAsync();
+                var channelTask = channel.GetOrDownloadAsync();
+                // データダウンロードされるまで待機する
+                await Task.WhenAll(messageTask, channelTask);
+                h(new() { 
+                    Message = message,
+                    Channel = channel
+                });
+            });
+        }
+    }
+}
