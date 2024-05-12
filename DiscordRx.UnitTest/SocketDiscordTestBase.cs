54765f4df1fc97c254b840cc098e412d9e3210c1
SHiNKi <shinki.5ktri@gmail.com>
2024-05-12 21:57:45 +0900

    テストコード追加

---
+++ DiscordRx.UnitTest/SocketDiscordTestBase.cs
@@ -0,0 +1,39 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordRx.UnitTest
+{
+    internal abstract class SocketDiscordTestBase : IClassFixture<AppSettingsFixture>, IDisposable
+    {
+        private readonly AppSettingsFixture _appsettingsFixture;
+
+        private string token;
+
+        protected DiscordSocketClient Client { get; init; }
+
+        public SocketDiscordTestBase(AppSettingsFixture appsettingsFixture)
+        {
+            _appsettingsFixture = appsettingsFixture;
+            token = _appsettingsFixture.Configuration.GetSection("testing")?.GetSection("token")?.ToString()!;
+
+            Client = new DiscordSocketClient(new DiscordSocketConfig()
+            {
+                GatewayIntents = Discord.GatewayIntents.All,
+            });
+        }
+
+        protected async Task LoginAsync()
+        {
+            await Client.LoginAsync(Discord.TokenType.Bot, token);
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+        }
+    }
+}
