e3c47074047b102e83a891a902d694f67afe6f03
SHiNKi <shinki.5ktri@gmail.com>
2024-05-12 22:24:50 +0900

    DiscordSocketClient.Ready

---
+++ DiscordRx/Extensions/DiscordSocket/ReadyEx.cs
@@ -0,0 +1,33 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordRx.Extensions.DiscordSocket
+{
+    /// <summary>
+    /// <seealso cref="DiscordSocketClient.Ready"/> 拡張メソッド
+    /// </summary>
+    public static class ReadyEx
+    {
+        /// <summary>
+        /// 準備完了した時に通知されます。
+        /// </summary>
+        /// <param name="socketClient"></param>
+        /// <returns></returns>
+        public static IObservable<Unit> NotifyReady(this DiscordSocketClient socketClient)
+        {
+            return Observable.FromEvent<Func<Task>, Unit>(h => () =>
+            {
+                h(Unit.Default);
+                return Task.CompletedTask;
+            },
+            h => socketClient.Ready += h,
+            h => socketClient.Ready -= h);
+        }
+    }
+}
