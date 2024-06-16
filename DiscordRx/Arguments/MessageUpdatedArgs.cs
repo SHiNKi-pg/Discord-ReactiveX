058e9391d3e41bf35351adc0b16ffe632489cc5e
SHiNKi <shinki.5ktri@gmail.com>
2024-06-16 13:19:58 +0900

    IDownloadableのファイル移動

--- DiscordRx/Arguments/MessageUpdatedArgs.cs
+++ DiscordRx/Arguments/MessageUpdatedArgs.cs
@@ -1,5 +1,6 @@
 ﻿using Discord;
 using Discord.WebSocket;
+using DiscordRx.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- DiscordRx/Core/IDownloadable.cs
+++ DiscordRx/Core/IDownloadable.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DiscordRx
+namespace DiscordRx.Core
 {
     /// <summary>
     /// ダウンロード可能インターフェース
